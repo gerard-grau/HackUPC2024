7de7566a4516e1b371663b094e3b3817f8ddde62
Pol Resina <pol.resina.martinez@estudiantat.upc.edu>
2024-05-04 20:26:35 +0200

    plantilla

--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 # server.py
-
+from werkzeug.utils import secure_filename
 import os
 from flask import Flask, render_template, request, redirect, url_for
 
@@ -12,17 +12,23 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def index():
     return render_template('upload_photo.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/display_photo/<filename>')
+def display_photo(filename):
+    return render_template('display_photo.html', filename=filename)
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_photo():
-    if 'photo' not in request.files:
+    file_key = 'photo' if 'photo' in request.files else 'gallery_photo'
+    if file_key not in request.files:
         return redirect(request.url)
-    photo = request.files['photo']
+    photo = request.files[file_key]
     if photo.filename == '':
         return redirect(request.url)
     if photo:
         # Save the uploaded file to the destination folder
+        filename = secure_filename(photo.filename)
         photo.save(os.path.join(app.config['UPLOAD_FOLDER'], photo.filename))
-        return f'Photo "{photo.filename}" uploaded successfully!'
+        return redirect(url_for('display_photo', filename=filename))
     return 'Error uploading photo.'
 
 if __name__ == "__main__":
